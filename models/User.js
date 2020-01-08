9048f27caf6d69368ce954377deca7f439d6d041
J. KrBaIO3 <krbaio3@gmail.com>
2020-01-08 22:36:16 +0100

    basic example

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const {
+    Schema,
+    model
+} = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    avatar: {
+        type: String,
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    favorites: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        ref: 'Post'
+    }
+})
+
+module.exports = model('User', UserSchema);
