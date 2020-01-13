76b8fb67f497d454cf4507b27eb0479e73dae884
J. KrBaIO3 <krbaio3@gmail.com>
2020-01-13 13:35:05 +0100

    Client App Bar

--- packages/client/src/plugins/vuetify.ts
+++ packages/client/src/plugins/vuetify.ts
@@ -2,11 +2,14 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/dist/vuetify.min.css';
 import colors from 'vuetify/es5/util/colors';
-
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg',
+  },
   theme: {
     themes: {
       light: {
