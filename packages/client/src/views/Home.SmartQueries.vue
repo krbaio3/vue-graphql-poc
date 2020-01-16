6506bed075863730ac4fd9763fb72ba3c461291a
J. KrBaIO3 <krbaio3@gmail.com>
2020-01-16 11:33:38 +0100

    Vue Apollo config and basic example

---
+++ packages/client/src/views/Home.SmartQueries.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container class="mt-5">
+    <h1>Home</h1>
+    <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{result: { loading, error, data, networkStatus } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occurred</div>
+        <!-- NetworkStatus -->
+        <!-- <div v-else-if="networkStatus" class="networkStatus apollo">Network Status: {{networkStatus}}</div> -->
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">
+          <ul
+            v-for="(post) in data.getPosts"
+            :key="post._id"
+          >
+            <li>
+              - {{post._id}}
+              - {{post.title}}
+              - {{post.imageUrl}}
+              - {{post.description}}
+            </li>
+          </ul>
+        </div>
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+        </div>
+      </template>
+    </ApolloQuery>
+  </v-container>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import { gql } from 'apollo-boost';
+@Component({
+  name: 'home',
+})
+export default class Home extends Vue {
+
+  private getPostsQuery() {
+   return gql `
+          {
+            getPosts {
+            _id
+            title
+            imageUrl
+            description
+          }
+        }`;
+      }
+}
+</script>
--- packages/client/src/views/Home.vue
+++ packages/client/src/views/Home.vue
@@ -1,29 +1,36 @@
 <template>
-  <v-container class="mt-5">
-    <h1>Home</h1>
-    <ApolloQuery :query="getPostsQuery">
-      <template slot-scope="{result: { loading, data } }">
-        <div v-if="loading">
-          ...Loadig
-        </div>
-        <div v-else>
-          {{data}}
-        </div>
-      </template>
-    </ApolloQuery>
+  <v-container
+    class="mt-6 text-center"
+    v-if="getPosts"
+  >
+    <v-flex xs12>
+      <v-carousel
+        v-bind="{'cycle': true}"
+        interval="3000"
+      >
+        <v-carousel-item
+          v-for="(post) in getPosts"
+          :key="post._id"
+          :src="post.imageUrl"
+        >
+          <h1
+            id="carousel__title"
+          >{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import { gql } from 'apollo-boost';
-@Component({
-  name: 'home',
-})
-export default class Home extends Vue {
 
-  private getPostsQuery() {
-   return gql `
+@Component({
+    name: 'Home',
+    apollo: {
+      getPosts: {
+        query: gql `
           {
             getPosts {
             _id
@@ -31,7 +38,24 @@ export default class Home extends Vue {
             imageUrl
             description
           }
-        }`;
-      }
-}
+        }`,
+      },
+    },
+  })
+  export default class AddPost extends Vue {
+    private getPosts = {};
+  }
 </script>
+<style lang="scss" scoped>
+#carousel__title{
+  position: absolute;
+  background-color: rgba(0,0,0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
